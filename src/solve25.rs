d5e2258dfcc09cf9c5c24addcfc2e337a4b7775b
Stanislav Golubtsov <sgaflv@gmail.com>
2020-06-09 13:47:39 +0200

    solve25 clean-up

--- src/solve25.rs
+++ src/solve25.rs
@@ -1,6 +1,6 @@
 extern crate num;
 
-use num::{BigInt, FromPrimitive, One};
+use num::{BigInt, FromPrimitive};
 
 
 pub fn solve25() -> u64 {
@@ -9,12 +9,12 @@ pub fn solve25() -> u64 {
     let mut n2: BigInt = FromPrimitive::from_usize(1).unwrap();
 
     for i in 1..10000 {
-        let sum: BigInt = n1.clone() + n2.clone();
+        let sum: BigInt = n1 + n2.clone();
 
-        n1 = n2.clone();
-        n2 = sum.clone();
+        n1 = n2;
+        n2 = sum;
 
-        let s =  n1.clone().to_str_radix(10);
+        let s =  n1.to_str_radix(10);
         if s.len() == 1000 {
             println!("{} {}", i, s);
             return i
